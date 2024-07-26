55ba2e7941a0809455d5a3f7b2110a2b50fa592f
sayyyho <323psh@naver.com>
2024-07-26 22:16:07 +0900

    Feat: footer 구현

--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -1,11 +1,12 @@
+import * as S from "./styled";
 import { Header } from "../../components/common/Header/Header";
 import { CustomCalendar } from "../../components/CustomCalendar/CustomCalendar";
 
 export const Calendar = () => {
   return (
-    <>
+    <S.Layout>
       <Header>캘린더</Header>
       <CustomCalendar />
-    </>
+    </S.Layout>
   );
 };
