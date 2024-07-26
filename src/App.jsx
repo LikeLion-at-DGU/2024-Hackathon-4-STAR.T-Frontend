06c7074b204c88f21faca4f0f8953c7977bc0c50
chaem03 <hcm0309@naver.com>
2024-07-26 13:17:34 +0900

    Feat: 메인 화면 컴포넌트 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import RoutineBox from "./components/RoutineBox";
+import CategoryTitle from "./components/CategoryTitle";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const routineData = {
+    title: "일주일에 한권 책읽기",
+    star: "BTS RM",
+    section: "도전중인 챌린지",
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RoutineBox title={routineData.title} star={routineData.star} />
+      <CategoryTitle section={routineData.section}></CategoryTitle>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/RoutineBox.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+const RoutineBoxContainer = styled.div`
+  display: flex;
+  width: 133px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 5px;
+`;
+
+const RoutineBoxImg = styled.div`
+  height: 100px;
+  align-self: stretch;
+  border-radius: 10px;
+  /* background: url(<path-to-image>) lightgray 50% / cover no-repeat; */
+  box-shadow: 0px 4px 7px 0px rgba(0, 0, 0, 0.25);
+  background-color: darkgray;
+`;
+
+const RoutineBoxTitle = styled.div`
+  color: var(--Font-Color, #2a2a2a);
+
+  /* Korean Body */
+  font-family: AppleSDGothicNeoL00;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const RoutineBoxStar = styled.div`
+  color: rgba(42, 42, 42, 0.7);
+
+  /* Korean Minimal */
+  font-family: AppleSDGothicNeoM00;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const RoutineBoxContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 5px;
+  align-self: stretch;
+`;
+
+function RoutineBox({ title, star }) {
+  return (
+    <RoutineBoxContainer>
+      <RoutineBoxImg></RoutineBoxImg>
+      <RoutineBoxContent>
+        <RoutineBoxTitle>{title}</RoutineBoxTitle>
+        <RoutineBoxStar>{star}</RoutineBoxStar>
+      </RoutineBoxContent>
+    </RoutineBoxContainer>
+  );
+}
+export default RoutineBox;
