98b01bada9c399f9d9e9356ce7890f15100c1fc4
sayyyho <323psh@naver.com>
2024-07-29 08:30:43 +0900

    Fix: 기록 페이지 개선 및 에러 수정

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -2,7 +2,7 @@ import * as S from "./style";
 
 export const Header = (props) => {
   return (
-    <S.Header>
+    <S.Header $padding={props.$padding} $margin={props.$margin}>
       <S.Title>{props.children}</S.Title>
     </S.Header>
   );
--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -5,7 +5,9 @@ import { CustomCalendar } from "../../components/CustomCalendar/CustomCalendar";
 export const Calendar = () => {
   return (
     <S.Layout>
-      <Header>캘린더</Header>
+      <Header $margin={"1rem 0 0 0"} $padding={"1rem 1rem 0 1rem"}>
+        캘린더
+      </Header>
       <CustomCalendar />
     </S.Layout>
   );
