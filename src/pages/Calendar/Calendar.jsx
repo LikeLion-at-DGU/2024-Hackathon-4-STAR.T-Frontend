e2d0859a4a1ee135b98aaebf22293162779bf6ae
sayyyho <323psh@naver.com>
2024-07-31 17:23:54 +0900

    Fix: 에러픽스

--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -1,8 +1,11 @@
 import * as S from "./styled";
 import { Header } from "../../components/common/Header/Header";
 import { CustomCalendar } from "../../components/CustomCalendar/CustomCalendar";
+import { getMonthCalenar } from "../../apis/calendar";
+import { useEffect } from "react";
 
 export const Calendar = () => {
+  useEffect(() => {}, []);
   return (
     <S.Layout>
       <Header $margin={"1rem 0 0 0"} $padding={"1rem 1rem 0 1rem"}>
