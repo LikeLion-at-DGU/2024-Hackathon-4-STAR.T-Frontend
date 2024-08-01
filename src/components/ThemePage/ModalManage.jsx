f3de4a895c0bcbd27761b3495c52b9cb803ee6c8
하채민 <163885086+Chaem03@users.noreply.github.com>
2024-08-01 21:16:49 +0900

    Add files via upload

---
+++ src/components/ThemePage/ModalManage.jsx
@@ -0,0 +1,49 @@
+import DateRangeCalendar from "../DateRangeCalendar/DateRangeCalendar";
+import { useState } from "react";
+import { CheckUp } from "../CheckUp/CheckUp";
+import Modal from "../Modal/Modal";
+
+export const ModalManage = ({
+  isCalendarVisible,
+  isCheckVisible,
+  closeAll,
+  handleCalendarConfirm: handleCalendarConfirmProp,
+  openCheck,
+}) => {
+  const [routineData, setRoutineData] = useState([]);
+
+  const handleCalendarConfirm = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const differenceInTime = end.getTime() - start.getTime();
+    const term = differenceInTime / (1000 * 3600 * 24) + 1;
+
+    setRoutineData({
+      start_date: startDate,
+      end_date: endDate,
+      duration_days: term,
+    });
+
+    openCheck();
+  };
+
+  return (
+    <>
+      {isCalendarVisible && (
+        <Modal onClose={closeAll}>
+          <DateRangeCalendar
+            onConfirm={(startDate, endDate) => {
+              handleCalendarConfirm(startDate, endDate);
+              handleCalendarConfirmProp(startDate, endDate);
+            }}
+          />
+        </Modal>
+      )}
+      {isCheckVisible && (
+        <Modal onClose={closeAll}>
+          <CheckUp onClose={closeAll} routineData={routineData} />
+        </Modal>
+      )}
+    </>
+  );
+};
---
+++ src/components/ThemePage/RoutineBoxContainer/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const RoutineBoxContainer = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  margin-bottom: 10rem;
+`;
