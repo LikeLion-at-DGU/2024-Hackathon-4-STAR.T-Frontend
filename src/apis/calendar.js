7c02a88016aa477a9b752c52989160b00954c4ed
sayyyho <323psh@naver.com>
2024-08-04 16:28:56 +0900

    Test: 퍼스널 패치 테스트

--- src/apis/calendar.js
+++ src/apis/calendar.js
@@ -25,6 +25,20 @@ export const postPersonal = async (title, description, date) => {
       title,
       description,
     });
+    if (res.status == 201 || res.status == 200) {
+      alert("등록완료!");
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const patchPersonal = async (personalID, isCompleted) => {
+  try {
+    const res = await instance.patch(`/api/calendar/daily/${date}/`, {
+      id: personalID,
+      completed: isCompleted,
+    });
     console.log(res);
   } catch (err) {
     throw err;
--- src/components/Todo/Item.jsx
+++ src/components/Todo/Item.jsx
@@ -2,19 +2,34 @@ import React from "react";
 import * as S from "./style";
 import NON_CHECK_IMG from "@/assets/images/non-check.svg";
 import CHECK_IMG from "@/assets/images/check.svg";
+import { patchPersonal } from "@/apis/calendar";
 
-export const Item = ({ item, isRoutine }) => (
-  <S.ListFrame key={item.id}>
-    <S.CheckFrame>
-      <S.ButtonView>
-        <S.ImgView src={item.completed ? CHECK_IMG : NON_CHECK_IMG} />
-      </S.ButtonView>
-    </S.CheckFrame>
-    <S.TextFrame>
-      <S.TitleView>{isRoutine ? item.routine_title : item.title}</S.TitleView>
-      <S.SubTitleView>
-        {isRoutine ? item.routine_content : item.description}
-      </S.SubTitleView>
-    </S.TextFrame>
-  </S.ListFrame>
-);
+export const Item = ({ item, isRoutine }) => {
+  const handleSubmit = async (isRoutine, id, isCompleted) => {
+    if (isRoutine) {
+      console.log("routine 로직 아직!");
+    } else {
+      await patchPersonal(id, !isCompleted);
+    }
+  };
+
+  return (
+    <S.ListFrame key={item.id}>
+      <S.CheckFrame>
+        <S.ButtonView
+          onClick={() => {
+            handleSubmit(isRoutine, item.id, item.completed);
+          }}
+        >
+          <S.ImgView src={item.completed ? CHECK_IMG : NON_CHECK_IMG} />
+        </S.ButtonView>
+      </S.CheckFrame>
+      <S.TextFrame>
+        <S.TitleView>{isRoutine ? item.routine_title : item.title}</S.TitleView>
+        <S.SubTitleView>
+          {isRoutine ? item.routine_content : item.description}
+        </S.SubTitleView>
+      </S.TextFrame>
+    </S.ListFrame>
+  );
+};
