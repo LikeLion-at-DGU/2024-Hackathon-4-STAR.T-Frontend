2610b4336afe0922990b50e15b3a5675ded9d023
sayyyho <323psh@naver.com>
2024-08-04 16:46:32 +0900

    Test: 퍼스널 패치 테스트

--- src/components/Todo/Item.jsx
+++ src/components/Todo/Item.jsx
@@ -6,20 +6,28 @@ import { patchPersonal } from "@/apis/calendar";
 
 export const Item = ({ item, isRoutine, date }) => {
   const handleSubmit = async (isRoutine, id, isCompleted) => {
-    if (isRoutine) {
-      console.log("routine 로직 아직!");
-    } else {
-      await patchPersonal(id, !isCompleted, date);
+    try {
+      if (isRoutine) {
+        console.log("routine 로직 아직!");
+      } else {
+        const response = await patchPersonal(id, !isCompleted, date);
+        if (response.status === 200) {
+          // API 호출 성공 시 상태 업데이트 로직 추가 (필요하다면)
+          console.log("Personal item updated successfully");
+        } else {
+          console.error("Failed to update personal item");
+        }
+      }
+    } catch (error) {
+      console.error("Error updating personal item:", error);
     }
   };
 
   return (
-    <S.ListFrame key={item.id}>
+    <S.ListFrame>
       <S.CheckFrame>
         <S.ButtonView
-          onClick={() => {
-            handleSubmit(isRoutine, item.id, item.completed);
-          }}
+          onClick={() => handleSubmit(isRoutine, item.id, item.completed)}
         >
           <S.ImgView src={item.completed ? CHECK_IMG : NON_CHECK_IMG} />
         </S.ButtonView>
