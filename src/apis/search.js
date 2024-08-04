31948e9657ca82b8d79ddb03e3326ef8a644e2d7
chaem03 <hcm0309@naver.com>
2024-08-04 19:48:44 +0900

    Test: 검색페이지 test

--- src/apis/search.js
+++ src/apis/search.js
@@ -2,7 +2,8 @@ import { instance } from "./instance";
 
 export const getSearchContent = async (data) => {
   try {
-    const res = await instance.get(`/api/search?data=${data}/`);
+    const res = await instance.get(`/api/search?data=${data}`);
+    console.log("getSearchContent에서 가져온 데이터:", res);
     return res;
   } catch (err) {
     console.log(err);
