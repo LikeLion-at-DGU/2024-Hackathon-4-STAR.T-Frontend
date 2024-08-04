7d7c81a5e10f9c9048b5e8650ee037bf8f58180e
sayyyho <323psh@naver.com>
2024-08-04 16:43:39 +0900

    Test: 퍼스널 패치 테스트

--- src/apis/calendar.js
+++ src/apis/calendar.js
@@ -1,46 +1,43 @@
-import { instance } from "./instance";
+import React from "react";
+import * as S from "./style";
+import NON_CHECK_IMG from "@/assets/images/non-check.svg";
+import CHECK_IMG from "@/assets/images/check.svg";
+import { patchPersonal } from "@/apis/calendar";
 
-export const getMonthCalendar = async (month) => {
-  try {
-    const res = await instance.get(`/api/calendar/monthly/${month}`);
-    return res.data;
-  } catch (err) {
-    throw err;
-  }
-};
-
-export const getTodayData = async (day) => {
-  try {
-    const res = await instance.get(`/api/calendar/daily/${day}/`);
-    return res.data;
-  } catch (err) {
-    throw err;
-  }
-};
-
-export const postPersonal = async (title, description, date) => {
-  console.log(date);
-  try {
-    const res = await instance.post(`/api/calendar/daily/${date}/`, {
-      title,
-      description,
-    });
-    if (res.status == 201 || res.status == 200) {
-      alert("등록완료!");
+export const Item = ({ item, isRoutine, date }) => {
+  const handleSubmit = async (isRoutine, id, isCompleted) => {
+    try {
+      if (isRoutine) {
+        console.log("routine 로직 아직!");
+      } else {
+        const response = await patchPersonal(id, !isCompleted, date);
+        if (response.status === 200) {
+          // API 호출 성공 시 상태 업데이트 로직 추가 (필요하다면)
+          console.log("Personal item updated successfully");
+        } else {
+          console.error("Failed to update personal item");
+        }
+      }
+    } catch (error) {
+      console.error("Error updating personal item:", error);
     }
-  } catch (err) {
-    throw err;
-  }
-};
+  };
 
-export const patchPersonal = async (personalID, isCompleted, date) => {
-  try {
-    const res = await instance.patch(`/api/calendar/daily/${date}/`, {
-      id: personalID,
-      completed: isCompleted,
-    });
-    console.log(res);
-  } catch (err) {
-    throw err;
-  }
+  return (
+    <S.ListFrame>
+      <S.CheckFrame>
+        <S.ButtonView
+          onClick={() => handleSubmit(isRoutine, item.id, item.completed)}
+        >
+          <S.ImgView src={item.completed ? CHECK_IMG : NON_CHECK_IMG} />
+        </S.ButtonView>
+      </S.CheckFrame>
+      <S.TextFrame>
+        <S.TitleView>{isRoutine ? item.routine_title : item.title}</S.TitleView>
+        <S.SubTitleView>
+          {isRoutine ? item.routine_content : item.description}
+        </S.SubTitleView>
+      </S.TextFrame>
+    </S.ListFrame>
+  );
 };
