3f8182d33a561cb524b8b6bf6acd1d3e346c441c
sayyyho <323psh@naver.com>
2024-08-06 04:53:34 +0900

    Fix: 등록 모달 수정

--- src/components/common/FinishAlert/FinishAlert.jsx
+++ src/components/common/FinishAlert/FinishAlert.jsx
@@ -25,13 +25,18 @@ const ModalContainer = styled.div`
   top: 0;
   transform: translateX(-50%);
   z-index: 1000;
-  width: 70%;
-  max-width: 350px;
   background: white;
   border-radius: 8px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   text-align: center;
   padding: 16px;
+  display: flex;
+  width: 240px;
+  height: 32px;
+  padding: 1px 38px;
+  justify-content: center;
+  align-items: center;
+
   animation: ${(props) => (props.state === "entering" ? slideDown : slideUp)}
     0.5s forwards;
 
@@ -44,22 +49,11 @@ const ModalContainer = styled.div`
   }
 `;
 
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
-  z-index: 999;
-`;
-
 export const AlertModal = ({ show, onClose }) => (
   <CSSTransition in={show} timeout={500} classNames="modal" unmountOnExit>
     <div>
-      <Overlay onClick={onClose} />
       <ModalContainer state={show ? "entering" : "exiting"}>
-        <p>작업이 완료되었습니다.</p>
+        <p>등록 완료 되었습니다.</p>
       </ModalContainer>
     </div>
   </CSSTransition>
