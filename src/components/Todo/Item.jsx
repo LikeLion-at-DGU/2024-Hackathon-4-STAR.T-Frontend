b95c07b24df5f125044719c4864d3cf413f58544
sayyyho <323psh@naver.com>
2024-08-05 21:44:52 +0900

    Fix: 캘린더 체크 시 체크 박스 변환

--- src/components/Todo/Item.jsx
+++ src/components/Todo/Item.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import * as S from "./style";
 import NON_CHECK_IMG from "@/assets/images/non-check.svg";
+import { useState } from "react";
 import CHECK_IMG from "@/assets/images/check.svg";
 import { patchPersonal, patchRoutine } from "@/apis/calendar";
-
+// 백에서 갯수 만큼 false true처럼 넣어야 할듯
 export const Item = ({ item, isRoutine, date }) => {
-  const handleSubmit = async (isRoutine, id, isCompleted) => {
+  const [isCompleted, setIsCompletd] = useState(item.completed);
+  const handleSubmit = async (isRoutine, id) => {
+    setIsCompletd(!isCompleted);
     try {
       if (isRoutine) {
-        const res = await patchRoutine(id, !isCompleted, date);
+        const res = await patchRoutine(id, isCompleted, date);
+        console.log(res);
       } else {
-        const response = await patchPersonal(id, !isCompleted, date);
+        const response = await patchPersonal(id, isCompleted, date);
+        console.log(res);
       }
     } catch (error) {
       console.error("Error updating personal item:", error);
@@ -20,10 +25,8 @@ export const Item = ({ item, isRoutine, date }) => {
   return (
     <S.ListFrame>
       <S.CheckFrame>
-        <S.ButtonView
-          onClick={() => handleSubmit(isRoutine, item.id, item.completed)}
-        >
-          <S.ImgView src={item.completed ? CHECK_IMG : NON_CHECK_IMG} />
+        <S.ButtonView onClick={() => handleSubmit(isRoutine, item.id)}>
+          <S.ImgView src={isCompleted ? CHECK_IMG : NON_CHECK_IMG} />
         </S.ButtonView>
       </S.CheckFrame>
       <S.TextFrame>
