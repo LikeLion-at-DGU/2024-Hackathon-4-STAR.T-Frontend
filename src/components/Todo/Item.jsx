310ede9c9f995f68179abacbf057759b46b94830
sayyyho <323psh@naver.com>
2024-08-05 23:41:20 +0900

    Fix: 캘린더 체크 시 체크 박스 변환

--- src/components/Todo/Item.jsx
+++ src/components/Todo/Item.jsx
@@ -15,28 +15,39 @@ export const Item = ({ item, isRoutine, date }) => {
       (checkItem) => checkItem.id === item.id && checkItem.date === date
     );
 
-    if (item.completed && !isItemChecked) {
-      setCheckItems((prevItems) => [...prevItems, { id: item.id, date }]);
+    if (!isItemChecked) {
+      setCheckItems((prevItems) => [
+        ...prevItems,
+        { id: item.id, date, isCompeted: item.completed },
+      ]);
     }
   }, [item, date, checkItems, setCheckItems]);
 
-  const handleSubmit = async (isRoutine, id, isChecked) => {
+  const handleSubmit = async (isRoutine, id, currentStatus) => {
     try {
+      // API 호출
       let res;
       if (isRoutine) {
-        res = await patchRoutine(id, isChecked, date);
+        res = await patchRoutine(id, currentStatus, date);
       } else {
-        res = await patchPersonal(id, isChecked, date);
+        res = await patchPersonal(id, currentStatus, date);
       }
 
-      // 서버 응답이 성공적일 경우 상태 업데이트
-      setCheckItems((prevItems) =>
-        isChecked
-          ? [...prevItems, { id, date }]
-          : prevItems.filter(
-              (checkItem) => !(checkItem.id === id && checkItem.date === date)
-            )
-      );
+      // 상태 업데이트
+      setCheckItems((prevItems) => {
+        const itemIndex = prevItems.findIndex(
+          (checkItem) => checkItem.id === id && checkItem.date === date
+        );
+
+        // 항목이 이미 존재하면 업데이트, 없으면 추가
+        if (itemIndex > -1) {
+          const updatedItems = [...prevItems];
+          updatedItems[itemIndex] = { id, date, isCompeted: currentStatus };
+          return updatedItems;
+        } else {
+          return [...prevItems, { id, date, isCompeted: currentStatus }];
+        }
+      });
 
       console.log(res);
     } catch (error) {
@@ -44,17 +55,21 @@ export const Item = ({ item, isRoutine, date }) => {
     }
   };
 
-  const isItemChecked = checkItems.some(
-    (checkItem) => checkItem.id === item.id && checkItem.date === date
+  // 현재 체크 상태
+  const isChecked = checkItems.some(
+    (checkItem) =>
+      checkItem.id === item.id &&
+      checkItem.date === date &&
+      checkItem.isCompeted
   );
 
   return (
     <S.ListFrame>
       <S.CheckFrame>
         <S.ButtonView
-          onClick={() => handleSubmit(isRoutine, item.id, !isItemChecked)}
+          onClick={() => handleSubmit(isRoutine, item.id, !isChecked)}
         >
-          <S.ImgView src={isItemChecked ? CHECK_IMG : NON_CHECK_IMG} />
+          <S.ImgView src={isChecked ? CHECK_IMG : NON_CHECK_IMG} />
         </S.ButtonView>
       </S.CheckFrame>
       <S.TextFrame>
