1f806452273738fb38e276b61dbfd3a55a696f23
sayyyho <323psh@naver.com>
2024-08-06 05:13:24 +0900

    Fix: 등록 모달 수정

--- src/components/common/FinishAlert/FinishAlert.jsx
+++ src/components/common/FinishAlert/FinishAlert.jsx
@@ -23,23 +23,21 @@ const slideUp = keyframes`
 const ModalLayout = styled.div`
   position: fixed;
   top: 0;
+  left: 50%;
+  transform: translateX(-50%);
   width: 100%;
-  height: 40px;
-  padding: 1px 38px;
   display: flex;
   justify-content: center;
-  align-items: center;
   z-index: 1000;
 `;
 
 const ModalContainer = styled.div`
-  width: 50%;
-  transform: translateX(-50%);
-  z-index: 1000;
+  width: fit-content;
   background: white;
   border-radius: 8px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   text-align: center;
+  padding: 8px 16px;
 
   animation: ${(props) => (props.state === "entering" ? slideDown : slideUp)}
     0.5s forwards;
