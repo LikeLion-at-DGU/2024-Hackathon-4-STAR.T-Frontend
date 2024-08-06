7e28bfba617519fe712443c2b86d415e806c0741
sayyyho <323psh@naver.com>
2024-08-06 15:24:24 +0900

    Feat: 스타페이지 에러 수정

--- src/hooks/useStar.js
+++ src/hooks/useStar.js
@@ -1,17 +1,24 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getStarContent } from "../apis/starP";
+import { useSetRecoilState } from "recoil";
+import { useNavigate } from "react-router-dom";
+import { isLoading } from "@/stores/loading";
 
 export const useMoveonStarP = () => {
+  const setLoading = useSetRecoilState(isLoading);
   const [starP, setstarP] = useState(null);
   const { id } = useParams();
   const fetchStarData = async () => {
+    setLoading(true);
     try {
       const res = await getStarContent(id);
       console.log("response:", res);
       setstarP(res);
     } catch (error) {
       console.error("error:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
