f212ff25b197f74fafa8a01157f60e0140ac2964
chaem03 <hcm0309@naver.com>
2024-08-06 17:14:28 +0900

    Design: footer 색상변경

--- src/pages/Share/Share.jsx
+++ src/pages/Share/Share.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import html2canvas from "html2canvas";
 import { captureScreenshot } from "@/utils/share";
 import * as S from "./styled";
